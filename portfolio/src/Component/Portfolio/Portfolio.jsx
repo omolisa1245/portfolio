f04a38714faa5bd2d558ca4afeb1ea78bc1fd8d6
olayemi omolisa <olayemiomolisa@yahoo.com>
2026-05-29 23:26:39 +0100

    styling correction

--- portfolio/src/Component/Portfolio/Portfolio.jsx
+++ portfolio/src/Component/Portfolio/Portfolio.jsx
@@ -146,11 +146,11 @@ const Portfolio = () => {
       <PortfolioSection
         title="Social Media Platform"
         description="A social media platform inspired by Instagram, TikTok, and WhatsApp, focused on performance, scalability, and seamless user experience across mobile and web platforms. Built with real-time messaging, stories, media sharing, authentication, notifications, and responsive cross-platform architecture."
-        images={[soc1, soc2, soc3, soc4, soc5,soc6,soc7,]}
+        images={[soc1, soc2, soc3, soc4, soc5,soc6,]}
       />
 
       <div className="w-full lg:w-[50%] px-4 -ml-5">
-        <div className="max-w-4xl -mt-50 mx-auto space-y-6">
+        <div className="max-w-4xl -mt-40 mx-auto space-y-6">
 
           {/* GitHub */}
           <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between gap-4 bg-white/10 backdrop-blur-md p-6 rounded-2xl">
